df503a488a7038aed526836af68f936a1ab249fa
Jafnak <jafnakusman@gmail.com>
2024-06-07 12:52:24 +0530

    AddBook component  added

---
+++ src/components/Add.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">BookTitle</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Category</label>
+                            <select name="" id="" className="form-control">
+                                <option value="Autobiography">Autobiography</option>
+                                <option value="Biography">Biography</option>
+                                <option value="Fiction">Fiction</option>
+                            </select>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Published Year</label>
+                            <input type="date" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Author</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Description</label>
+                            <textarea name="" id="" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Distributor Name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Publisher name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Price</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label"></label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">Add Book</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Add
