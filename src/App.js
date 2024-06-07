1243400351481072e311ebed84e41bdc4c5200b6
Jafnak <jafnakusman@gmail.com>
2024-06-07 12:57:11 +0530

    SearchBook component  added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
+import SearchBook from './components/SearchBook';
 
 function App() {
   return (
     <div>
      <Add/>
+     <SearchBook/>
 
       
     </div>
