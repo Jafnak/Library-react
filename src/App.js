6e916672d92e4e7a5fa29f518f8d0e89cf6dd6c9
Jafnak <jafnakusman@gmail.com>
2024-06-07 13:00:41 +0530

    DeleteBook component  added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
 import SearchBook from './components/SearchBook';
+import DeleteBook from './components/DeleteBook';
 
 function App() {
   return (
     <div>
      <Add/>
      <SearchBook/>
+     <DeleteBook/>
 
       
     </div>
---
+++ src/components/DeleteBook.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const DeleteBook = () => {
+  return (
+    <div>
+
+        <div className="container">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">BookTitle</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg col-xl-12 col-xxl-12">
+                    <button className="btn btn-danger">Delete</button>
+                </div>
+            </div>
+        </div>
+        <br></br>
+    </div>
+  )
+}
+
+export default DeleteBook
